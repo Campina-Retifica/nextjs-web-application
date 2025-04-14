f436a737543953d24f2bf25968093e37d1b3e9ae
Batissta <francinaldofilho05@gmail.com>
2025-04-14 16:05:11 -0300

    feat: signIn login

--- src/components/form/LoginForm.tsx
+++ src/components/form/LoginForm.tsx
@@ -1,60 +1,67 @@
 "use client";
-import React from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Button } from "../ui/button";
 import Error from "@/components/helpers/Error";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { loginSchema, TLoginSchema } from "../types/LoginSchema";
+import { authUser } from "@/services/auth/auth";
+import { useRouter } from "next/navigation";
 
 const LoginForm = () => {
   const {
     register,
     reset,
-    resetField,
     handleSubmit,
     formState: { errors, isSubmitting },
   } = useForm<TLoginSchema>({ resolver: zodResolver(loginSchema) });
+  const [error, setError] = useState<string>("");
+  const router = useRouter();
   return (
     <form
-      className="flex flex-col poppins w-[100%] md:w-[90%] mb-8"
-      onSubmit={handleSubmit((data) => {
-        console.log(data);
+      className="flex flex-col poppins w-[100%] md:w-[90%] mb-4"
+      onSubmit={handleSubmit(async (data) => {
+        setError("");
+        const success = await authUser(data);
+        if (success) {
+          return router.push("/");
+        }
+        setError("Credênciais Inválidas");
         reset();
-        resetField("nome");
-        resetField("senha");
       })}
     >
       <div className="flex flex-col mb-1">
-        <label htmlFor="nome" className="flex flex-col">
+        <label htmlFor="username" className="flex flex-col">
           Nome
           <input
-            {...register("nome")}
+            {...register("username")}
             type="text"
-            id="nome"
-            name="nome"
+            id="username"
+            name="username"
             className="text-zinc-700 duration-200 bg-zinc-200 p-2 rounded outline-none border border-zinc-400 hover:border-zinc-950"
             autoComplete="off"
           />
         </label>
-        <Error error={errors.nome && errors.nome.message} />
+        <Error error={errors.username && errors.username.message} />
       </div>
 
       <div className="flex flex-col mb-1">
-        <label htmlFor="senha" className="flex flex-col">
+        <label htmlFor="password" className="flex flex-col">
           Senha
           <input
-            {...register("senha")}
+            {...register("password")}
             type="password"
-            id="senha"
-            name="senha"
+            id="password"
+            name="password"
             className="text-zinc-700 duration-200 bg-zinc-200 p-2 rounded outline-none border border-zinc-400 hover:border-zinc-950"
           />
         </label>
-        <Error error={errors.senha && errors.senha.message} />
+        <Error error={errors.password && errors.password.message} />
       </div>
       <Button disabled={isSubmitting} className="cursor-pointer mt-1 rounded">
         {!isSubmitting ? "Entrar" : "Carregando..."}
       </Button>
+      <Error error={error && error} />
     </form>
   );
 };
